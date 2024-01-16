48e441fd033a3074812eda28b308c2a8422b03e9
Aman Kumar Kushwaha <amansom155@gmail.com>
2024-01-16 18:35:45 +0530

    Added CustomException.py file

--- color_code.py
+++ color_code.py
@@ -1,7 +1,9 @@
+import sys
 from collections import Counter
 
 from PIL import Image
 
+from exception import CustomException
 from logger import logging
 from url_screenshot import PathConfig
 
@@ -12,27 +14,31 @@ class GetRGBValues:
         self.path = PathConfig
 
     def get_rgb(self) -> None:
-        logging.info("Getting RGB values ...")
-        image: Image = Image.open(self.path.image_path)
+        try:
+            logging.info("Getting RGB values ...")
+            image: Image = Image.open(self.path.image_path)
 
-        # Convert the image to a list of RGB tuples
-        pixels = list(image.getdata())
+            # Convert the image to a list of RGB tuples
+            pixels = list(image.getdata())
 
-        # Use Counter to count the occurrences of each color
-        color_counts = Counter(pixels)
+            # Use Counter to count the occurrences of each color
+            color_counts = Counter(pixels)
 
-        # Get the 6 most common colors
-        top_colors = color_counts.most_common(6)
+            # Get the 6 most common colors
+            top_colors = color_counts.most_common(6)
 
-        with open("RGB_code/values.txt", "w") as f:
-            for i, (color, count) in enumerate(top_colors):
-                color_block = "\033[48;2;{};{};{}m    \033[0m".format(
-                    color[0], color[1], color[2]
-                )
+            with open("RGB_code/values.txt", "w") as f:
+                for i, (color, count) in enumerate(top_colors):
+                    color_block = "\033[48;2;{};{};{}m    \033[0m".format(
+                        color[0], color[1], color[2]
+                    )
 
-                print(
-                    f"Color {i + 1}: {color_block} RGB: {color[:-1]} - Count: {count}"
-                )
+                    print(
+                        f"Color {i + 1}: {color_block} RGB: {color[:-1]} - Count: {count}"
+                    )
 
-                f.write(f"RGB: {color[:-1]} - Count: {count} \n")
-                logging.info(f"RGB: {color[:-1]} - Count: {count}")
+                    f.write(f"RGB: {color[:-1]} - Count: {count} \n")
+                    logging.info(f"RGB: {color[:-1]} - Count: {count}")
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
--- url_screenshot.py
+++ url_screenshot.py
@@ -1,14 +1,18 @@
+import sys
 from dataclasses import dataclass
 
-from logger import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+
+from exception import CustomException
+from logger import logging
 
 
 @dataclass
 class PathConfig:
     image_path: str = "Screenshots/screenshot.png"
+    # Give you own path 
     chrome_driver_path: str = "C:/Users/aman kushwaha/Desktop/The/Web Development/chromedriver-win64/chromedriver.exe"
 
 
@@ -18,9 +22,14 @@ class GetScreenshots:
         self.paths = PathConfig()
         self.s = Service(self.paths.chrome_driver_path)
 
-    def get_screenshots(self, url: str) -> None:
-        driver = webdriver.Chrome(service=self.s)
-        driver.get(url)
-        driver.save_screenshot(self.paths.image_path)
-        driver.quit()
-        logging.info("Get the screenshot Successfully...")
+    try:
+
+        def get_screenshots(self, url: str) -> None:
+            driver = webdriver.Chrome(service=self.s)
+            driver.get(url)
+            driver.save_screenshot(self.paths.image_path)
+            driver.quit()
+            logging.info("Get the screenshot Successfully...")
+
+    except Exception as e:
+        raise CustomException(e, sys) from e
