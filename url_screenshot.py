9f4f8043c57762da6729018884366f9bdfdf0022
Aman Kumar Kushwaha <amansom155@gmail.com>
2024-01-16 20:18:42 +0530

    update url_screenshot

--- url_screenshot.py
+++ url_screenshot.py
@@ -12,7 +12,7 @@ from logger import logging
 @dataclass
 class PathConfig:
     image_path: str = "Screenshots/screenshot.png"
-    # Give you own path 
+    # Give you own path
     chrome_driver_path: str = "C:/Users/aman kushwaha/Desktop/The/Web Development/chromedriver-win64/chromedriver.exe"
 
 
@@ -22,14 +22,15 @@ class GetScreenshots:
         self.paths = PathConfig()
         self.s = Service(self.paths.chrome_driver_path)
 
-    try:
-
-        def get_screenshots(self, url: str) -> None:
-            driver = webdriver.Chrome(service=self.s)
+    def get_screenshots(self, url: str) -> None:
+        try:
+            option = Options()
+            option.add_argument("--headless")
+            driver = webdriver.Chrome(service=self.s, options=option)
             driver.get(url)
             driver.save_screenshot(self.paths.image_path)
             driver.quit()
             logging.info("Get the screenshot Successfully...")
 
-    except Exception as e:
-        raise CustomException(e, sys) from e
+        except Exception as e:
+            raise CustomException(e, sys) from e
