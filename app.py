4732812f3ef35341149697341cf17737d0b1fb9b
Aman Kumar Kushwaha <amansom155@gmail.com>
2024-01-16 21:54:15 +0530

    flask app integrated

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+from flask import Flask, render_template
+
 from color_code import GetRGBValues
 from url_screenshot import GetScreenshots
 
+app = Flask(__name__)
 
-def main():
-    ss = GetScreenshots()
-    ss.get_screenshots("https://www.python.org/")
-    obj = GetRGBValues()
-    obj.get_rgb()
+
+@app.route("/")
+def index():
+    # ss = GetScreenshots()
+    # ss.get_screenshots("https://www.python.org/")
+    val = GetRGBValues()
+    colors_info = val.get_rgb()
+    return render_template("index.html", colors_info=colors_info)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
